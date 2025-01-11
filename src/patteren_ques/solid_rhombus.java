4002ce897e0a77281f31448a204541788aa53c0c
Arsh <amarjitsingh345876@gmail.com>
2025-01-11 21:19:48 +0530

    solved two more problems

--- src/patteren_ques/solid_rhombus.java
+++ src/patteren_ques/solid_rhombus.java
@@ -3,16 +3,17 @@ package patteren_ques;
 public class solid_rhombus {
     public static void main(String[] args){
         int n = 5;
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=0;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for(int j=0;j<=n-i;j++){
+            //rhombus
+            for(int j=1;j<=5;j++){
                 System.out.print("*");
             }
-            for(int j=1;j<=i-1;j++){
-                System.out.print("*");
-            }
+
+
             System.out.println(" ");
         }
     }
