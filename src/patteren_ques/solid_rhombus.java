52a2def9e49062634c3944e805222bc89f2adcba
Arsh <amarjitsingh345876@gmail.com>
2025-01-11 17:06:35 +0530

    solved three more pattern questions

---
+++ src/patteren_ques/solid_rhombus.java
@@ -0,0 +1,19 @@
+package patteren_ques;
+
+public class solid_rhombus {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
