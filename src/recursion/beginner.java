9de4d8ee5f962d37154eb982879686a0b47e0c83
Arsh <amarjitsingh345876@gmail.com>
2025-02-04 23:35:58 +0530

    try solving problem of recursion

--- src/recursion/beginner.java
+++ src/recursion/beginner.java
@@ -2,6 +2,14 @@ package recursion;
 
 public class beginner {
     public static void main(String[] args){
-
+        //Print numbers from 1 to 5.
+        printNumbers(5);
+    }
+    public static void printNumbers(int n) {
+        if(n == 6) {
+            return;
+        }
+        System.out.println(n);
+        printNumbers(n+1);
     }
 }
