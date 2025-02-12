d62c655c683a957e9dfe7a64d43125d8e8b61fae
Arsh <amarjitsingh345876@gmail.com>
2025-02-12 21:22:06 +0530

    recursion

--- src/recursion/beginner.java
+++ src/recursion/beginner.java
@@ -2,15 +2,26 @@ package recursion;
 
 public class beginner {
     public static void main(String[] args){
-        //Print numbers from 1 to 5.
-        printNumbers(5);
+//        int n=5;
+        int n=1;
+//        printnumb(n);
+        printno(n);
+
     }
-    public static void printNumbers(int n) {
-        if(n == 5) {
+//    public static void printnumb(int n){
+//        if (n==0){
+//            return;
+//        }
+//        System.out.println(n);
+//        printnumb(n-1);
+//    }
+
+    // print numbers from 1 to 5
+    public static void printno(int n){
+        if(n==6){
             return;
         }
-        /// //////hbyubunhuhiouj
         System.out.println(n);
-        printNumbers(n+1);
+        printno(n+1);
     }
 }
